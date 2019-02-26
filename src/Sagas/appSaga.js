ace4682d33759fac8ce34357dc8bad82e968eb9f
Raj A <raj_a@photonint.com>
2019-02-26 17:14:44 +0530

    'changes'

--- src/Sagas/appSaga.js
+++ src/Sagas/appSaga.js
@@ -2,10 +2,6 @@ import { put, takeEvery, all } from 'redux-saga/effects'
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
-// function* helloSaga() {
-//   console.log('Hello Sagas!')
-// }
-
 // export function* incrementAsync() {
 //   yield delay(1000)
 //   yield put({ type: 'INCREMENT' })
@@ -19,7 +15,6 @@ export function* watchIncrementAsync() {
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield all([
-    // helloSaga(),
     watchIncrementAsync()
   ])
 }
